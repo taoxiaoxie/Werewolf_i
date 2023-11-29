18cfe46d40dc0d441d29a8f9fdecef78e665e960
buleboy <2252125665@qq.com>
2023-11-29 16:54:44 +0800

    updatae

---
+++ Werewolf/environments/umshini/__init__.py
@@ -0,0 +1,7 @@
+from .debate import DebateEnv, create_debate_env
+from .pettingzoo_wrapper import PettingZooCompatibilityV0
+from .symmetric_content_moderation import (
+    SymmetricContentModerationEnv,
+    create_content_moderation_env,
+)
+from .symmetric_deception import SymmetricDeceptionEnv, create_deception_env
